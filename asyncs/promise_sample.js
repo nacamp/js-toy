6886e628f8536fe8b773d052c054beee59aa8fae
najimmy <najimmy@gmail.com>
2022-04-01 16:57:35 +0900

    [test] promise

--- asyncs/promise_sample.js
+++ asyncs/promise_sample.js
@@ -1,6 +1,34 @@
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+// util.promisify()
+// https://nodejs.org/dist/latest-v8.x/docs/api/util.html#util_util_promisify_original
+// https://flaviocopes.com/node-promisify/
+const fs = require('fs');
+const util = require('util');
+
+// case1: util.promisify
+fs.readFile('./index.js', 'utf8', (err, text) => {
+  if (err) {
+    console.log('Error1>>', err);
+  } else {
+    console.log(text);
+  }
+});
+const readFile = util.promisify(fs.readFile);
+
+async function doFile() {
+  try {
+    const text = await readFile('./index.js', 'utf8');
+    console.log(text);
+  } catch (err) {
+    console.log('Error2>>', err);
+  }
+}
+doFile();
+
+// case2: promise.all
 const p1 = Promise.resolve(3);
 const p2 = 1337;
-const p5Resolve = new Promise((resolve, reject) => {
+const p5Resolve = new Promise((resolve) => {
   setTimeout(() => {
     console.log('p5Resolve finished');
     resolve('p5Resolve finished');
@@ -10,11 +38,12 @@ const p1Resolve = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('p1Resolve finished');
     resolve('p1Resolve finished');
-  }, 1000);
+    // reject(new Error('sorry222'));
+  }, 2000);
 });
 const p1Reject = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reject('sorry');
+    reject(new Error('sorry'));
   }, 1000);
 });
 
@@ -26,15 +55,18 @@ const p1Reject = new Promise((resolve, reject) => {
     console.log(r);
   } catch (e) {
     console.log('catch>>');
-    console.log(e);
   }
   console.log('end...');
 }());
+// 에러는 캐치후 end지만, 나머지 all안에 있는 작업도 비동기로 모두 실행됨
 /*
-out:
-p1Resolve finished
 catch>>
-sorry
+Error: sorry
+    at Timeout._onTimeout (/Users/jimmy/WebstormProjects/js-toy/asyncs/promise_sample.js:45:12)
+    at listOnTimeout (internal/timers.js:554:17)
+    at processTimers (internal/timers.js:497:7)
+end.2222..
 end...
+p1Resolve finished
 p5Resolve finished
 */
