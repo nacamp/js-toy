500b073c37b37e4ad6fe081a4fba492a9af60e91
jimmy-at-147941222526 <8nYny11JGC9a3TPeazgByab14xD/vFo8bUm1+QX7INU=>
2022-06-23 14:19:56 +0900

    test array & typescript

--- language/functional.sample.js
+++ language/functional.sample.js
@@ -1,56 +1,91 @@
 // https://www.toptal.com/javascript/functional-programming-javascript
 // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 // https://velog.io/@grinding_hannah/JavaScript-Functional-Programming%ED%95%A8%EC%88%98%ED%98%95-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D
-let result;
-const add = (a, b) => a + b;
-console.log(add(1, 2));
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+// let result;
+// const filterEven = (x) => x % 2 === 0;
+// result = [1, 2, 3].filter(filterEven);
+// console.log(result);
+//
+// const double = (x) => 2 * x;
+// result = [1, 2, 3].map(double);
+// console.log(result);
+//
+function arrayFrom() {
+  const range = (n) => Array.from(Array(n).keys());
+  console.log(range(3));
+  const rangeWithStep = (start, stop, step) => (
+    Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step))
+  );
+  console.log(rangeWithStep(0, 2, 1));
+}
 
-let idCount = 0;
-const getId = () => ++idCount;
-console.log(getId());
+function arrayReduce() {
+  console.log('reduce...');
+  let result;
 
-const filterEven = (x) => x % 2 === 0;
-result = [1, 2, 3].filter(filterEven);
-console.log(result);
+  // case default
+  const sum = (accumulatedSum, arrayItem) => accumulatedSum + arrayItem;
+  result = [1, 2, 3].reduce(sum);
+  console.log(result);
 
-const double = (x) => 2 * x;
-result = [1, 2, 3].map(double);
-console.log(result);
-
-const sum = (accumulatedSum, arrayItem) => accumulatedSum + arrayItem;
-result = [1, 2, 3].reduce(sum);
-console.log(result);
-
-const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
-result = names.reduce((allNames, name) => {
-  if (name in allNames) {
-    allNames[name]++;
-    console.log(1, allNames);
-  } else {
+  // case array 변경
+  const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+  result = names.reduce((allNames, name) => {
     allNames[name] = 1;
-    console.log(2, allNames);
-  }
-  return allNames;
-}, {});
-console.log(result);
+    console.log(`allNames=${JSON.stringify(allNames)} name=${name}`);
+    return allNames;
+  }, {});
+  console.log(result);
 
-result = [1, 2].concat([3, 4]);
-console.log(result);
+  // case break
+  const array = ['12', '34', '56', '78', '99'];
+  const x = array.reduce((acc, curr, i, arr) => {
+    if (i === 2) arr.splice(1); // eject early
+    // eslint-disable-next-line no-return-assign
+    return acc += curr;
+  }, '');
+  console.log('x: ', x); // x:  123456
 
-const obj = { a: 2 };
-result = Object.assign({}, obj);
-console.log(result);
-console.log({ ...obj });
+  // case break
+  const array2 = [12, 34, 56, 78, 99];
+  const x2 = array2.reduce((acc, curr, i, arr) => {
+    if (i === 2) arr.splice(1); // eject early
+    // eslint-disable-next-line no-return-assign
+    console.log(i, acc, curr);
+    acc.push(curr);
+    return acc;
+  }, []);
+  console.log('x2: ', x2); // x:  123456
+}
 
-// Higher-order Functions
-const withLog = (fn) => {
-  return (...args) => {
+function concat() {
+  // array, copy
+  let result;
+  result = [1, 2].concat([3, 4]);
+  console.log(result);
+
+  // object
+  const obj = { a: 2 };
+  result = { ...obj };
+  console.log(result);
+  console.log({ ...obj });
+}
+
+function higherOrderFunction() {
+  let result;
+  const add = (a, b) => a + b;
+  console.log(add(1, 2));
+
+  // Higher-order Functions
+  const withLog = (fn) => (...args) => {
     console.log(`calling ${fn.name}`);
     return fn(...args);
   };
-};
-const addWithLogging = withLog(add);
-result = addWithLogging(3, 4);
-console.log(result);
-result = withLog(add)(3, 4);
-console.log(result);
+  const addWithLogging = withLog(add);
+  result = addWithLogging(3, 4);
+  console.log(result);
+  result = withLog(add)(3, 4);
+  console.log(result);
+}
+higherOrderFunction();
---
+++ typescript/sample.ts
@@ -0,0 +1,3 @@
+// string to number
+const nid: number = +"10";
+console.log(nid);
